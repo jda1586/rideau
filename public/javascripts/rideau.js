8e558ea51d3284bc8525cb7bc1b5cc02d3e555f7
Alejandro del Rio <adelriosantiago@gmail.com>
2016-09-01 11:50:33 -0500

    Fix collections zoomable images

--- public/javascripts/rideau.js
+++ public/javascripts/rideau.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('#Container').mixItUp();
-    
-    var elements = $('.zoomable-image');
+
+	var elements = $('.zoomable-image');
 	console.log(elements);
 	if (elements.length) Intense(elements);
 	
