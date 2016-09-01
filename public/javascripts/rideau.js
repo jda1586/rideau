6a13e93aab72bc6e71ec05608a910a887145ade7
Alejandro del Rio <adelriosantiago@gmail.com>
2016-09-01 17:00:15 -0500

    WIP about to change all pages to the new header

--- public/javascripts/rideau.js
+++ public/javascripts/rideau.js
@@ -4,24 +4,4 @@ $(document).ready(function() {
 	var elements = $('.zoomable-image');
 	console.log(elements);
 	if (elements.length) Intense(elements);
-	
-	$(document).mousemove(function(e) {
-		var height;
-		if (document.body) {
-			height = (document.body.clientHeight);
-		} else {
-			height = (window.innerHeight);
-		}
-		
-		/* console.log(e.pageY - $(window).scrollTop()); */
-		if ((e.pageY - $(window).scrollTop()) < 150 /*|| (e.pageY - $(window).scrollTop()) > (height - 60)*/ ) { //The bottom limit is commented, uncomment to enable that functionality
-			$('header').removeClass("hidden");
-			$('#r-logo').addClass("hidden");
-			//$('footer').removeClass("hidden"); //No longer used, footer is always visible
-		} else {
-			$('header').addClass("hidden");
-			$('#r-logo').removeClass("hidden");
-			//$('footer').addClass("hidden"); //No longer used, footer is always visible
-		}
-	});
 });
