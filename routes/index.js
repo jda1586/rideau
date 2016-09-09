efaba617081eb573918f009128484ae1b60fe98a
Alejandro del Rio <adelriosantiago@gmail.com>
2016-09-09 11:34:44 -0500

    WIP Broken

--- routes/index.js
+++ routes/index.js
@@ -75,6 +75,7 @@ router.get('/eboutique/:name?', function(req, res, next) {
 	data = _.compact(_.map(data, function(el) {
 		if (el.enabled === "1") return el;
 	}));
+<<<<<<< HEAD
 	
 	console.log(data);
 	
@@ -85,6 +86,16 @@ router.get('/eboutique/:name?', function(req, res, next) {
 			data.images.male = _.omit(data.images.male, 'featured'); //Omit the featured image (only shown en eboutique page)
 			data.images.female = _.omit(data.images.female, 'featured'); //Omit the featured image (only shown en eboutique page)
 		
+=======
+	
+	if (!_.isUndefined(name)) {
+		data = data[name];
+		
+		if (!_.isUndefined(data)) {
+			data.images.male = _.omit(data.images.male, 'featured'); //Omit the featured image (only shown en eboutique page)
+			data.images.female = _.omit(data.images.female, 'featured'); //Omit the featured image (only shown en eboutique page)
+		
+>>>>>>> dropbox/master
 			return res.render('detail', { data: data });
 		}
 	}
