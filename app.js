0930510e98225ccebb46996bbea68942d74ab87b
Alejandro del Rio <adelriosantiago@gmail.com>
2016-09-10 13:35:56 -0500

    File Jan

--- app.js
+++ app.js
@@ -5,64 +5,67 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
+var middleware = require('./routes/middleware');
 var routes = require('./routes/index');
 
 var app = express();
 
-// view engine setup
+//View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
+//Uncomment after placing your favicon in /public
+
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev', {
-    skip: function (req, res) {
-        return res.statusCode < 400; //Only log errors
-    }
+	skip: function (req, res) {
+		return res.statusCode < 400; //Only log errors
+	}
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({
-    src: path.join(__dirname, '/public/scss'),
-    dest: path.join(__dirname, '/public/stylesheets'),
-    debug: false,
-    prefix: '/stylesheets',
-    outputStyle: 'compressed'
+	src: path.join(__dirname, '/public/scss'),
+	dest: path.join(__dirname, '/public/stylesheets'),
+	debug: false,
+	prefix: '/stylesheets',
+	outputStyle: 'compressed'
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-// catch 404 and forward to error handler
+//Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
+//Error handlers
 
-// development error handler
-// will print stacktrace
+//Development error handler
+//Will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
-// production error handler
-// no stacktraces leaked to user
+//Production error handler
+//No stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 module.exports = app;
