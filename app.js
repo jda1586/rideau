80d11f5ca9ffd022c4e8ad52275699916a6dc9f9
Alejandro del Rio <adelriosantiago@gmail.com>
2016-09-20 13:17:28 -0500

    Add express session and add to middleware

--- app.js
+++ app.js
@@ -25,6 +25,11 @@ app.use(logger('dev', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(session({
+	secret: "de0f92987b8b6e7337898b8e894846ac",
+	resave: true, 
+	saveUninitialized: false
+}));
 app.use(sassMiddleware({
 	src: path.join(__dirname, '/public/scss'),
 	dest: path.join(__dirname, '/public/stylesheets'),
