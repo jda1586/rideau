8e63fffdeefece433d09f3a0b005d6389384cf55
Alejandro del Rio <adelriosantiago@gmail.com>
2016-09-21 22:40:42 -0500

    Fix subscribe modal padding

--- public/plugins/sweetalert2/sweetalert2.js
+++ public/plugins/sweetalert2/sweetalert2.js
@@ -75,8 +75,8 @@
     timer: null,
     width: 500,
     //padding: 20,
-    paddingTopBottom: 20,
-    paddingLeftRight: 90,
+    //paddingTopBottom: 20,
+    //paddingLeftRight: 90,
     background: '#FFF',
     input: null, // 'text' | 'email' | 'password' | 'select' | 'radio' | 'checkbox' | 'textarea' | 'file'
     inputPlaceholder: '',
@@ -457,10 +457,13 @@
     }
 
     //modal.style.padding = params.padding + 'px';
-    modal.style.paddingTop = params.paddingTopBottom + 'px';
+    
+	//Disabled (Please, do not enable! Use SCSS instead)
+	/*modal.style.paddingTop = params.paddingTopBottom + 'px';
     modal.style.paddingBottom = params.paddingTopBottom + 'px';
 	modal.style.paddingLeft = params.paddingLeftRight + 'px';
-    modal.style.paddingRight = params.paddingLeftRight + 'px';
+    modal.style.paddingRight = params.paddingLeftRight + 'px';*/
+	
     //modal.style.background = params.background; //Disabled
 
     if (widthUnits === 'px') {
